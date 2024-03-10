96bca2682e79e19699157a3f1b25126003b1fcee
sardor-dev1 <sardorprogramist@gmail.com>
2024-03-10 17:21:45 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 const toggleBtn = document.getElementById("toggleBtn");
 const menu = document.getElementById("menu");
 
-let open = false;
-
 toggleBtn.addEventListener("click", () => {
-  open = !open;
-  if (open) {
-    menu.classList.add("block");
-    menu.classList.remove("hidden");
-  } else {
-    menu.classList.add("hidden");
-    menu.classList.remove("block");
-  }
+  menu.classList.toggle("translate-x-[-100%]");
 });
